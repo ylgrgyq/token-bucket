df5e2bbedc6cc5f20ac5ebef1768944f9ee10106
Rui Guo <rguo@leancloud.rocks>
2015-12-20 10:08:20 +0800

    (test) memory based token bucket

--- src/main/scala/com/token_bucket/MemoryBasedTokenBucket.scala
+++ src/main/scala/com/token_bucket/MemoryBasedTokenBucket.scala
@@ -8,13 +8,15 @@ import java.util.concurrent.TimeUnit
   */
 class MemoryBasedTokenBucket(capacity: Int, interval: Long, minInterval: Long = 0, unit: TimeUnit = TimeUnit.SECONDS) extends TokenBucket {
   require(capacity > 0, "Bucket Capacity should bigger than 0")
+  require(interval > 0, "Interval time should bigger than 0")
   require(minInterval >= 0, "Minimum interval time should not negative")
-  require(interval >= 0, "Interval should not negative")
+  require(unit != null, "Please give me time unit for parameter minInterval and interval")
 
   private val tokenSupplyPolicy = new FixedRateTokenSupplyPolicy(Math.ceil(capacity.toDouble / interval).toLong, 1, unit)
 
   private var tokensCount = capacity
   private var lastConsumeTime = 0L
+  private val minIntervalInNanos = unit.toNanos(minInterval)
 
   private def refill(now: Long, tokensSupplied: Int) = {
     tokensCount = math.min(capacity, tokensSupplied + tokensCount)
@@ -27,7 +29,8 @@ class MemoryBasedTokenBucket(capacity: Int, interval: Long, minInterval: Long = 
     require(tokenInNeed > 0)
 
     val now = System.nanoTime()
-    if (minInterval != 0 && now - lastConsumeTime < minInterval) {
+
+    if (minIntervalInNanos != 0 && now - lastConsumeTime < minIntervalInNanos) {
       false
     } else {
       refill(now, tokenSupplyPolicy.supplyToken(now))
--- src/test/scala/com/token_bucket/MemoryBasedTokenBucketSuite.scala
+++ src/test/scala/com/token_bucket/MemoryBasedTokenBucketSuite.scala
@@ -1,46 +1,105 @@
 package com.token_bucket
 
-
 import java.util.concurrent.TimeUnit
 
 import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
-
 @RunWith(classOf[JUnitRunner])
-class MemoryBasedTokenBucketSuite extends FunSuite{
+class MemoryBasedTokenBucketSuite extends FunSuite {
 
-  test("tryConsume return false after buckets is drained"){
-    val capacity = 30
-    val interval = 5
+  trait TestBucket {
+    val (capacity, interval, u) = (30, 5, TimeUnit.SECONDS)
     val v = new MemoryBasedTokenBucket(capacity, interval, unit = TimeUnit.SECONDS)
-
-    val range = new Range(0, 30, 1)
-    for (i <- range) {
-      assert(v.tryConsume())
-    }
-
-    assert(! v.tryConsume())
   }
 
-  test("drain bucket then wait half interval the bucket should refill some tokens"){
-    val capacity = 30
-    val interval = 5
-    val v = new MemoryBasedTokenBucket(capacity, interval, unit = TimeUnit.SECONDS)
+  trait WithMinIntervalTestBucket extends TestBucket {
+    val minInter = 1
+    override val v = new MemoryBasedTokenBucket(capacity, interval, minInterval = minInter, unit = TimeUnit.SECONDS)
+  }
 
-    val range = new Range(0, 30, 1)
-    for (i <- range) {
+  test("negative capacity") {
+    val (capacity, interval) = (-1, 1)
+    intercept[IllegalArgumentException] {
+      new MemoryBasedTokenBucket(capacity, interval)
+    }
+  }
+
+  test("zero capacity") {
+    val (capacity, interval) = (0, 1)
+    intercept[IllegalArgumentException] {
+      new MemoryBasedTokenBucket(capacity, interval)
+    }
+  }
+
+  test("negative interval"){
+    val (capacity, interval) = (1, -1)
+    intercept[IllegalArgumentException] {
+      new MemoryBasedTokenBucket(capacity, interval)
+    }
+  }
+
+  test("zero interval") {
+    val (capacity, interval) = (1, 0)
+    intercept[IllegalArgumentException] {
+      new MemoryBasedTokenBucket(capacity, interval)
+    }
+  }
+
+  test("negative minInterval"){
+    val (capacity, interval, minInterval) = (1, 1, -1)
+    intercept[IllegalArgumentException] {
+      new MemoryBasedTokenBucket(capacity, interval, minInterval = minInterval)
+    }
+  }
+
+  test("null time unit"){
+    val (capacity, interval, u) = (1, 1, null)
+    intercept[IllegalArgumentException] {
+      new MemoryBasedTokenBucket(capacity, interval, unit = u)
+    }
+  }
+
+  test("tryConsume return false after buckets is drained") {
+    new TestBucket {
+      val range = new Range(0, 30, 1)
+      for (i <- range) {
+        assert(v.tryConsume())
+      }
+
+      assert(!v.tryConsume())
+    }
+  }
+
+  test("drain bucket then wait half interval the bucket should refill some tokens") {
+    new TestBucket {
+      val range = new Range(0, 30, 1)
+      for (i <- range) {
+        assert(v.tryConsume())
+      }
+
+      assert(!v.tryConsume())
+
+      TimeUnit.NANOSECONDS.sleep(TimeUnit.SECONDS.toNanos(interval / 2))
+      for (i <- new Range(1, (((interval / 2).toDouble / interval) * capacity).toInt, 1)) {
+        assert(v.tryConsume())
+      }
+
+      assert(!v.tryConsume())
+    }
+  }
+
+  test("interval between two consecutive consumption should bigger than minInterval") {
+    new WithMinIntervalTestBucket {
+      assert(v.tryConsume())
+      assert(!v.tryConsume())
+
+      TimeUnit.SECONDS.sleep(1)
+
+      assert(v.tryConsume())
+      TimeUnit.SECONDS.sleep(1)
       assert(v.tryConsume())
     }
-
-    assert(! v.tryConsume())
-
-    TimeUnit.NANOSECONDS.sleep(TimeUnit.SECONDS.toNanos(interval / 2))
-    for (i <- new Range(1, (((interval / 2).toDouble / interval) * capacity).toInt, 1)){
-      assert(v.tryConsume())
-    }
-
-    assert(! v.tryConsume())
   }
 }
